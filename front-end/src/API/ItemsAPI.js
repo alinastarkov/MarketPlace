e7a0d701010f3069361e85b87adbc0359e3328ef
alinastarkov <hayleeluu@gmail.com>
2020-03-21 18:42:32 -0400

    API change

--- front-end/src/API/ItemsAPI.js
+++ front-end/src/API/ItemsAPI.js
@@ -5,3 +5,31 @@ export function getItems() {
   const url = `${API_URL}/api/items/`;
   return axios.get(url).then(response => response.data);
 }
+
+export function postItem(data) {
+  const config = {
+    headers: {
+      "Content-Type": "multipart/form-data"
+    }
+  };
+  const url = `${API_URL}/sell-item/`;
+  let form_data = new FormData();
+  form_data.append("image", data.image);
+  form_data.append("name", data.name);
+  form_data.append("category", data.category);
+  form_data.append("description", data.description);
+  form_data.append("size", data.size);
+  form_data.append("price", data.price);
+  form_data.append("brand", data.brand);
+  form_data.append("username", data.username);
+
+  return axios.post(url, form_data, config).then(
+    response => {
+      return response.data;
+    },
+    error => {
+      console.log(error);
+      return { error: true };
+    }
+  );
+}
