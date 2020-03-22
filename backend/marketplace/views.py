4d5df9f467c2322f852bc2fe781a5cfc47f40078
alinastarkov <hayleeluu@gmail.com>
2020-03-21 22:44:34 -0400

    add new views

--- backend/marketplace/views.py
+++ backend/marketplace/views.py
@@ -21,6 +21,7 @@ def current_user(request):
     serializer = UserSerializer(request.user)
     return Response(serializer.data)
 
+#todo: SAME TOKEN!!!???
 class UserList(APIView):
     permission_classes = (permissions.AllowAny,)
     def post(self, request, format=None):
@@ -33,12 +34,18 @@ class UserList(APIView):
 class ItemView(APIView):
     #to do: change to authentication token can access only
     permission_classes = (permissions.AllowAny,)
+    def get(self, request, format=None):
+        name=request.query_params.get("username")
+        item = Item.objects.all().filter(user__username=name)
+        serializer = ItemSerializer(item, many=True)
+        return Response(serializer.data)
 
     def post(self, request, format=None):
         parser_classes = (MultiPartParser, FormParser)
         serializer = ItemSerializer(data=request.data)
         if serializer.is_valid():
             name = request.data.get("username")
+            print(name)
             userInstance = User.objects.get(username=name)
             serializer.save(user = userInstance)
             return Response(serializer.data, status=status.HTTP_201_CREATED)
